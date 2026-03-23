c0f4a7845bd16bc75ff5026a6f7c640b63e37a97
inaneda <edainann@gmail.com>
2026-03-23 22:21:58 +0300

    feat: ADC reading for LDR and MQ135, UART debug added

--- main.c
+++ main.c
@@ -18,30 +18,29 @@ int main(void) {
 	gpio_init();
 	adc_init();
 	uart_init(UBRR_VAL);
-
+	
 	uint16_t ldr_val, mq135_val;
-
+	
 	while (1) {
 		ldr_val   = adc_read(ADC_LDR);
 		mq135_val = adc_read(ADC_MQ135);
-
+		
 		uart_send_string("LDR: ");
 		uart_send_uint16(ldr_val);
 		uart_send_string(" | MQ135: ");
 		uart_send_uint16(mq135_val);
 		uart_send_string("\r\n");
-
+		
 		if (ldr_val < 512) {
 			led_set(0, 0, 1);
 			} else {
 			led_set(1, 0, 0);
 		}
-
+		
 		if (mq135_val > 700) {
 			buzzer_on();
 		}
-
+		
 		_delay_ms(500);
 	}
 }
-
